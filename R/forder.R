1a6380fa569ced3be82e9e031804a3d687d90ce6
Alex Peterson <alex.castonpeterson@gmail.com>
2015-11-20 13:35:32 -0800

    add desc sorting ability to forder

--- R/forder.R
+++ R/forder.R
@@ -2,18 +2,20 @@
 #'
 #'This function reorders a factor in a data.frame based
 #'off of another (quanitative) variable in that data.frame. The default function
-#'is mean, although other functions can be passed.
+#'is mean, although other functions can be passed. And the default behavior is
+#'descending order, although it can be changed to ascending with desc = FALSE.
 #'
-#' @param df
-#' @param factor
-#' @param var
+#' @param df dataframe
+#' @param factor factor
+#' @param var variable for sorting
+#' @param desc logical
 #'
 #' @return factor
 #' @export
 #' @examples
-#' iris$Species <- forder(iris, iris$Species, iris$Sepal.Width)
+#' iris$Species <- forder(iris, iris$Species, iris$Sepal.Width, desc = FALSE)
 #' boxplot(Sepal.Width ~ Species, data = iris)
-forder <- function(df, factor, var, fun = mean) {
+forder <- function(df, factor, var, fun = mean, desc = TRUE) {
   if (class(df) != "data.frame") {
     stop("Error: this function can only take data.frames as input\n",
          "You have provided an object of class ", class(df))
@@ -21,5 +23,10 @@ forder <- function(df, factor, var, fun = mean) {
   if (nlevels(df) == 1) {
     stop("Error: your factor only has 1 level, no reason to reorder.")
   }
-  factor <- stats::reorder(factor, var, fun)
+  if (desc == TRUE) {
+  return(factor <- dplyr::desc(stats::reorder(factor, var, fun)))
+  }
+  else {
+  return(factor <- stats::reorder(factor, var, fun))
+}
 }
